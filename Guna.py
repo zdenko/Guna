12e877a619219acc6b85d13279a2e408b2014124
Zdenko <zdenko.vujasinovic@gmail.com>
2022-12-08 05:38:40 +0100

    transit

--- Guna.py
+++ Guna.py
@@ -11,25 +11,26 @@ import sys, imp
 import traceback
 
 ##  sub-modules  ______________________________________________
-try:
-    # reload
-    mods = ['Guna.core.persist', 'Guna.core.api', 'Guna.core.engine']
-    for mod in mods:
-        if any(mod == m for m in list(sys.modules)):
-            imp.reload(sys.modules[mod])
-    # import
-    from .core import persist
-    from .core import api
-    from .core.api import GunaApi
-    from .core import engine
-    from .core.engine import (GunaEventListener, GunaSwitchWidget, GunaSetTheme, GunaTweakTheme, GunaTweakWidget, GunaToggleFullScreen, GunaReadme, GunaIssue, GunaUpscaleIcon)
-    import_ok = True
-except Exception:
-    print ('GUNA : ERROR ________________________________________________')
-    traceback.print_exc()
-    print ('=============================================================')
-    import_ok = False
+# try:
+#     # reload
+#     mods = ['Guna.core.persist', 'Guna.core.api', 'Guna.core.engine']
+#     for mod in mods:
+#         if any(mod == m for m in list(sys.modules)):
+#             imp.reload(sys.modules[mod])
+#     # import
+#     from .core import persist
+#     from .core import api
+#     from .core.api import GunaApi
+#     from .core import engine
+#     from .core.engine import (GunaEventListener, GunaSwitchWidget, GunaSetTheme, GunaTweakTheme, GunaTweakWidget, GunaToggleFullScreen, GunaReadme, GunaIssue, GunaUpscaleIcon)
+#     import_ok = True
+# except Exception:
+#     print ('GUNA : ERROR ________________________________________________')
+#     traceback.print_exc()
+#     print ('=============================================================')
+#     import_ok = False
 
+import_ok = False
 # package control
 try:
     from package_control import events
@@ -41,11 +42,11 @@ except Exception:
 
 def plugin_loaded():
     # import
-    if not import_ok:
-        sublime.status_message("* GUNA : Error in importing sub-modules. Please, see the trace-back message in Sublime console")
-        return
+    # if not import_ok:
+    #     sublime.status_message("* GUNA : Error in importing sub-modules. Please, see the trace-back message in Sublime console")
+    #     return
 
-    engine.start()
+    # engine.start()
     return
     if package_control_installed and (events.install('Guna') or events.post_upgrade('Guna')):
         def installed():
@@ -62,15 +63,16 @@ def plugin_loaded():
 
         # sublime.set_timeout_async(installed, 1000)
     else:
+      return
         # engine start
-        engine.start()
+        # engine.start()
     return
 
 ##  plugin_unloaded  __________________________________________
 
 def plugin_unloaded():
     # engine stop
-    if package_control_installed:
-        if events.remove('Guna') or events.pre_upgrade('Guna'):
-            engine.stop()
+    # if package_control_installed:
+    #     if events.remove('Guna') or events.pre_upgrade('Guna'):
+    #         engine.stop()
     return
